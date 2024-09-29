553f0fef6872d17f5d30618673c9bd2bb4d1dc60
DucHo <hohuuduc2709@gmail.com>
2024-09-29 18:07:39 +0700

    Add project files.

---
+++ SingletonService/Singleton.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Test")]
+
+namespace SingletonService
+{
+    internal class Singleton
+    {
+        private static readonly Dictionary<Type, object> _instances = [];
+
+        public static T GetInstance<T>()
+        {
+            var type = typeof(T);
+            return (T)GetInstance(type);
+        }
+
+        public static object GetInstance(Type type)
+        {
+            var instance = _instances.GetValueOrDefault(type);
+            if (instance == null)
+            {
+                try
+                {
+                    instance = CreateInstance(type);
+                }
+                catch (NullableException)
+                {
+                    throw new Exception("The type must not be nullable");
+                }
+                _instances.Add(type, instance);
+            }
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Register an instance for the class; if it has already been registered, it will automatically override the previous instance.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <exception cref="NullReferenceException"></exception>
+        public static void Register<T>(T obj)
+        {
+            if (obj != null)
+                _instances[typeof(T)] = obj;
+            else
+                throw new NullReferenceException("The parameter must not to be null");
+        }
+
+        private static object CreateInstance(Type type)
+        {
+            var constructors = type.GetConstructors();
+            if (constructors.Length == 0)
+                throw new ZeroIndexException();
+            foreach (var constructor in constructors)
+            {
+                try
+                {
+                    var parameters = constructor.GetParameters();
+                    if (parameters.Length == 0)
+                    {
+                        var instance = Activator.CreateInstance(type);
+                        if (instance != null)
+                            return instance;
+                        else
+                            throw new NullableException();
+                    }
+                    else
+                    {
+                        var instances = new object[parameters.Length];
+                        for (int i = 0; i < parameters.Length; i++)
+                        {
+                            instances[i] = CreateInstance(parameters[0].ParameterType);
+                        }
+                        var instance = Activator.CreateInstance(type, instances);
+                        if (instance != null)
+                            return instance;
+                        else
+                            throw new NullableException();
+                    }
+                }
+                catch (ZeroIndexException)
+                {
+                    continue;
+                }
+            }
+            throw new MissingMethodException();
+        }
+    }
+
+    class ZeroIndexException : Exception { }
+    class NullableException : Exception { }
+}
---
+++ Test/SingletonCase.cs
@@ -0,0 +1,72 @@
+﻿using SingletonService;
+
+namespace Test
+{
+    [TestClass]
+    public class SingletonCase
+    {
+        [TestMethod]
+        public void Method1()
+        {
+            Singleton.Register(new B());
+            Singleton.Register(new C());
+            var i = Singleton.GetInstance<B>();
+            Assert.AreEqual(typeof(B), i.GetType());
+        }
+
+        [TestMethod]
+        public void Method2()
+        {
+            var o1 = new C() { N = 1 };
+            var o2 = new C() { N = 2 };
+            Singleton.Register(o1);
+            Singleton.Register(o2);
+            var i = Singleton.GetInstance<C>();
+            Assert.AreEqual(i.N, 2);
+            Assert.AreNotEqual(i.N, 1);
+        }
+
+        [TestMethod]
+        public void Method3()
+        {
+            Singleton.Register(typeof(C));
+            var i = Singleton.GetInstance<C>();
+            Assert.IsNotNull(i);
+        }
+
+        [TestMethod]
+        public void Method4()
+        {
+            var i = Singleton.GetInstance<D>();
+            Assert.IsNotNull(i);
+        }
+
+        [TestMethod]
+        public void Method5()
+        {
+            var l = typeof(D).GetConstructors()[0].GetParameters();
+        }
+    }
+
+    abstract class A 
+    {
+        public abstract void F1();
+        public void F2() {}
+    }
+
+    class B : A
+    {
+        public override void F1(){}
+    }
+
+    class C : A
+    {
+        public int N;
+        public override void F1() { }
+    }
+
+    class D
+    {
+        public D(C c) { }
+    }
+}
